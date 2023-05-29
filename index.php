756fdc9108ac71ebc1f8946c69c227bbb965b335
BahaaY <104075963+BahaaY@users.noreply.github.com>
2023-05-29 12:56:58 +0300

    update index file

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
             </div>
 
             <div class="col">
-                <a href="Login.php" id="a_main">Login</a>
+                <a href="Login.php" id="a_main">Loginn</a>
             </div>
 
         </section>
